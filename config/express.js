e687ca1979ceeb83ce12c3f358d9fc29e36c93d8
TonniAndrade <tonni@agenciakindle.com.br>
2017-01-03 13:37:52 -0200

    [NEW] Creates basic server config with mongodb

---
+++ config/express.js
@@ -0,0 +1,44 @@
+/**
+ *.@license-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+ *.._______                _ _____
+ *.|__   __|              (_)  __ \  Created by
+ *....| | ___  _ __  _ __  _| |  | | _____   __
+ *....| |/ _ \| '_ \| '_ \| | |  | |/ _ \ \ / /
+ *....| | (_) | | | | | | | | |__| |  __/\ V /
+ *....|_|\___/|_| |_|_| |_|_|_____/ \___| \_/ v1.0
+ *03/01/17-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+ */
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const load = require('express-load');
+
+module.exports = function(){
+    let app = express();
+
+    app.set('port', process.env.PORT || 3000);
+
+    app.use(session({
+        secret: 'simpleauth',
+        resave: true,
+        saveUninitialized: true
+    }));
+
+    app.use(express.static('./public'));
+
+    app.set('views', './public');
+    app.set('views cache', true);
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(require('method-override')());
+
+    load('models', {cwd: 'app'})
+        .then('controllers')
+        .then('routes')
+        .into(app);
+
+    return app;
+};
---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+ *.@license-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+ *.._______                _ _____
+ *.|__   __|              (_)  __ \  Created by
+ *....| | ___  _ __  _ __  _| |  | | _____   __
+ *....| |/ _ \| '_ \| '_ \| | |  | |/ _ \ \ / /
+ *....| | (_) | | | | | | | | |__| |  __/\ V /
+ *....|_|\___/|_| |_|_| |_|_|_____/ \___| \_/ v1.0
+ *03/01/17-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+ */
+'use strict';
+
+const http = require('http');
+const app = require('./config/express')();
+require('./config/database')('mongodb://localhost/auth');
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('::Express server listening on port:: ' + app.get('port'));
+});
